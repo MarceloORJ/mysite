a04ad8b201608c054ac824fe265824ce057aedbc
Marcelo Oliveira <marcelo@exemplo.com>
2024-05-07 12:48:43 -0300

    update post_view / add templates

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -5,7 +5,7 @@ from blog.models.post import Post
 
 class PostView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'templates/index.html'
+    template_name = 'index.html'
 
 class PostDetail(generic.DetailView):
     model = Post
